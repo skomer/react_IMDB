95456b9c7b4378666b6814840f3ec910a1a42c15
Josephine Elder <parusmontanus@gmail.com>
2017-01-09 17:21:53 +0000

    Created 'GetShowtimes' react component

---
+++ client/src/components/GetShowtimes.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+
+
+var GetShowTimes = React.createClass({
+
+  render: function() {
+    return (
+      <div className='get-showtimes'>
+        <h3>Get Showtimes</h3>
+      </div>
+    );
+  }
+
+});
+
+module.exports = GetShowTimes;
