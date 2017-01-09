ae823b79a1ada2d335b13e41e99e683a8d34dbd3
Josephine Elder <parusmontanus@gmail.com>
2017-01-09 17:24:01 +0000

    Created 'FilmList' react component

---
+++ client/src/components/FilmList.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var FilmList = React.createClass({
+
+  render: function() {
+    return (
+      <div className='film-list'>
+        <h3>Film List</h3>
+      </div>
+    );
+  }
+
+});
+
+module.exports = FilmList;
