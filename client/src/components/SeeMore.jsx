b698599b6195d94024744a24d9b6e299e6b15c84
Josephine Elder <parusmontanus@gmail.com>
2017-01-09 17:26:51 +0000

    Created 'SeeMore' react component

---
+++ client/src/components/SeeMore.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var SeeMore = React.createClass({
+
+  render: function() {
+    return (
+      <div className='see-more'>
+        <h3>See More</h3>
+      </div>
+    );
+  }
+
+});
+
+module.exports = SeeMore;
